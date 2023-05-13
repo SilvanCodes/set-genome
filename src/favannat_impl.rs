ffc72a94af735410b7ad3f954d5a1c16f0058151
Silvan Buedenbender <silvancodes@gmail.com>
2023-05-13 02:15:34 +0200

    Update to favannat 0.6.2

--- src/favannat_impl.rs
+++ src/favannat_impl.rs
@@ -61,3 +61,112 @@ impl Recurrent<Node, Connection> for Genome {
         self.recurrent.as_sorted_vec()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use favannat::{MatrixRecurrentFabricator, StatefulEvaluator, StatefulFabricator};
+    use rand_distr::{Distribution, Uniform};
+
+    use crate::{activations::Activation, Genome, Mutations, Parameters, Structure};
+
+    // This test brakes with favannat version 0.6.1 due to a bug there. Now with favannat 0.6.2 it is fine.
+    #[test]
+    fn verify_output_does_not_occasionally_leak_internal_state() {
+        let parameters = Parameters {
+            structure: Structure {
+                number_of_inputs: 13,
+                number_of_outputs: 3,
+                percent_of_connected_inputs: 1.0,
+                outputs_activation: Activation::Sigmoid,
+                seed: 42,
+            },
+            mutations: vec![
+                Mutations::ChangeWeights {
+                    chance: 0.8,
+                    percent_perturbed: 0.5,
+                    standard_deviation: 0.2,
+                },
+                Mutations::AddNode {
+                    chance: 0.1,
+                    activation_pool: vec![
+                        Activation::Sigmoid,
+                        Activation::Tanh,
+                        Activation::Gaussian,
+                        Activation::Step,
+                        // Activation::Sine,
+                        // Activation::Cosine,
+                        Activation::Inverse,
+                        Activation::Absolute,
+                        Activation::Relu,
+                    ],
+                },
+                Mutations::AddConnection { chance: 0.2 },
+                Mutations::AddConnection { chance: 0.02 },
+                Mutations::AddRecurrentConnection { chance: 0.1 },
+                Mutations::RemoveConnection { chance: 0.05 },
+                Mutations::RemoveConnection { chance: 0.01 },
+                Mutations::RemoveNode { chance: 0.05 },
+            ],
+        };
+
+        let mut genome = Genome::initialized(&parameters);
+
+        for _ in 0..100 {
+            genome.mutate(&parameters);
+        }
+
+        let mut evaluator = MatrixRecurrentFabricator::fabricate(&genome).expect("not okay");
+
+        let between = Uniform::from(-10000.0..10000.0);
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..1000 {
+            let input = (0..13)
+                .map(|_| between.sample(&mut rng))
+                .collect::<Vec<_>>();
+            let output = evaluator.evaluate(input);
+            assert!(
+                output[0] <= 1.0,
+                "got {} which is bigger than 1.0, genome: {:?}, evaluator: {:?}",
+                output[0],
+                &genome,
+                &evaluator
+            );
+            assert!(
+                output[0] >= 0.0,
+                "got {} which is smaller than 0.0, genome: {:?}, evaluator: {:?}",
+                output[0],
+                &genome,
+                &evaluator
+            );
+            assert!(
+                output[1] <= 1.0,
+                "got {} which is bigger than 1.0, genome: {:?}, evaluator: {:?}",
+                output[1],
+                &genome,
+                &evaluator
+            );
+            assert!(
+                output[1] >= 0.0,
+                "got {} which is smaller than 0.0, genome: {:?}, evaluator: {:?}",
+                output[1],
+                &genome,
+                &evaluator
+            );
+            assert!(
+                output[2] <= 1.0,
+                "got {} which is bigger than 1.0, genome: {:?}, evaluator: {:?}",
+                output[2],
+                &genome,
+                &evaluator
+            );
+            assert!(
+                output[2] >= 0.0,
+                "got {} which is smaller than 0.0, genome: {:?}, evaluator: {:?}",
+                output[2],
+                &genome,
+                &evaluator
+            );
+        }
+    }
+}
